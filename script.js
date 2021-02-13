8374422bb29ed3af13372fc9c14408c85d5c935a
javanikeed <javanikeed@gmail.com>
2021-02-13 00:30:54 -0700

    Added pokemon type

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
-const sprite = document.getElementById('sprite')
-const pokemonName = document.getElementById('name')
-const button = document.getElementById('pokemonBtn')
-button.addEventListener('click', generatePokemon)
+const sprite = document.getElementById('sprite');
+const pokemonName = document.getElementById('name');
+const button = document.getElementById('pokemonBtn');
+const pokemonType = document.getElementById('type');
+button.addEventListener('click', generatePokemon);
 
+generatePokemon();
 
 function generateId() {
-    const min = 1
-    const max = 898
-    let randomNum = Math.random() * (max - min) + min;
-    return Math.floor(randomNum);
+  const min = 1;
+  const max = 151;
+  let randomNum = Math.random() * (max - min) + min;
+  return Math.floor(randomNum);
 }
 
-async function generatePokemon(){
-    const config = {
-        headers: {
-            "Content-Type": "application/json"
-        },
-    }
+async function generatePokemon() {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
 
-    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${generateId()}`, config)
+  const res = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${generateId()}`,
+    config
+  );
 
-    const data = await res.json()
+  const data = await res.json();
 
-    pokemonName.innerHTML = data.name
-    sprite.src = data.sprites.front_default
+  pokemonName.innerHTML = data.name;
+  //console.log(data.types[0].type.name);
+  pokemonType.innerHTML = data.types[0].type.name;
+  sprite.src = data.sprites.front_shiny;
 }
